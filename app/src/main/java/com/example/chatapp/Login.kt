8c87a76c613f21424e17deea19f0994f6df564ce
Surprise <mesanwoolasurprise303>
2022-01-26 07:44:33 -0800

    Updates

--- app/src/main/java/com/example/chatapp/Login.kt
+++ app/src/main/java/com/example/chatapp/Login.kt
@@ -3,8 +3,12 @@ package com.example.chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+
+private const val TAG = "Login"
 
 class Login : AppCompatActivity() {
 
@@ -18,11 +22,29 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnLogin = findViewById(R.id.btn_login)
         btnSignUp = findViewById(R.id.btn_signup)
+
         btnSignUp.setOnClickListener {
             val intent = Intent(this@Login, SignUp::class.java)
             startActivity(intent)
         }
+
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString().trim()
+            val password = edtPassword.text.toString().trim()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Enter your email and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            addUser(email, password)
+        }
+
+    }
+
+    private fun addUser(email: String, password: String) {
 
     }
 }
